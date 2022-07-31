ae5c6f47a6f86c09b462297d8b63691e7730838c
Bombowy <patryk.kulpok@vp.pl>
2022-07-31 17:13:19 +0200

    adding to games

--- src/routes/games/games.component.jsx
+++ src/routes/games/games.component.jsx
@@ -1,15 +1,22 @@
-import GameCard from '../../components/games-card/games-card.component'
+import GameCard from "../../components/games-card/games-card.component"
 
-import './games.styles.scss'
+import "./games.styles.scss"
 
 const Games = () => {
-    return(
-        <div className='games'><div className='games-container'>
-        <GameCard name='Memories' img={require('../../assets/memories.jpg')} link='https://bombowy.github.io/memories/'></GameCard>
-        
-        </div></div>
-        
-    )
+	return (
+		<div className='games'>
+			<div className='games-container'>
+				<GameCard
+					name='Memories'
+					img={require("../../assets/memories.jpg")}
+					link='https://bombowy.github.io/memories/'></GameCard>
+				<GameCard
+					name='Wisielec'
+					img={require("../../assets/wisielec.jpg")}
+					link='https://bombowy.github.io/wisielec/'></GameCard>
+			</div>
+		</div>
+	)
 }
 
 export default Games
